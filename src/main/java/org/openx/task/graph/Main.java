550541360b8172dd0f64cade8e6beb61e6b0491d
Illia Melnykov <91326814+ttdever@users.noreply.github.com>
2023-04-22 14:20:46 +0200

    init commit

---
+++ src/main/java/org/openx/task/graph/Main.java
@@ -0,0 +1,7 @@
+package org.openx.task.graph;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/openx/task/graph/classes/Node.java
@@ -0,0 +1,14 @@
+package org.openx.task.graph.classes;
+
+public class Node {
+
+    public Node(int value) {
+        this.value = value;
+    }
+    private int value;
+
+    private Node leftNode;
+    private Node rightNode;
+
+
+}
